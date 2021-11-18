b98a4d3d8ab412f7a4b8980fcd6d7b59b068f62a
Atila Santos <atila.romao@hotmail.com>
2021-11-18 19:47:41 -0300

    Implementing random messages to be returned by the greetings package

--- greetings.go
+++ greetings.go
@@ -3,12 +3,28 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomMessagem() string {
+	messages := []string{
+		"Hey %v, how are you doing!",
+		"Haaaooowwww %v, whats up!",
+		"Good to see you %v!",
+	}
+
+	return messages[rand.Intn(len(messages))]
+}
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hello, %v!", name)
+	message := fmt.Sprintf(randomMessagem(), name)
 	return message, nil
 }
